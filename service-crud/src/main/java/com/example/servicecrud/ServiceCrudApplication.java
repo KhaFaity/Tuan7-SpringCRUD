d5bba4a02baed0eaf00cec93a57fcf379fb7b036
KhaFairy <huynhduykha2001@gmail.com>
2022-10-24 11:24:58 +0700

    servicecrud

--- service-crud/src/main/java/com/example/servicecrud/ServiceCrudApplication.java
+++ service-crud/src/main/java/com/example/servicecrud/ServiceCrudApplication.java
@@ -1,16 +1,19 @@
-package App;
+package com.example.servicecrud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+@SpringBootApplication
+public class ServiceCrudApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCrudApplication.class, args);
+	}
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+
 }
